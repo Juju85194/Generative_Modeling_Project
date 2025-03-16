a564ada4186c2a3de90c0ca1d54e15329b3b71f7
QMoayedpour <quentin.moayedpour@ensae.fr>
2025-03-16 15:11:07 +0000

    add utils

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import torchvision
+from PIL import Image
+import torch
+
+
+def pilimg_to_tensor(pil_img, device="cuda"):
+    t = torchvision.transforms.ToTensor()(pil_img)
+    t = 2*t-1 # [0,1]->[-1,1]
+    t = t.unsqueeze(0)
+    t = t.to(device)
+    return(t)
+
+
+def display_as_pilimg(t):
+    t = 0.5+0.5*t.to('cpu')
+    t = t.squeeze()
+    t = t.clamp(0.,1.)
+    pil_img = torchvision.transforms.ToPILImage()(t)
+    display(pil_img)
+    return(pil_img)
